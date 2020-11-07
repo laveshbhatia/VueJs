198d440e8f37ed062a157ac16f362270501c3b4e
laveshbhatia <lavesh2001@gmail.com>
2020-11-07 07:09:27 +0400

    Added 10 products to test using for loop to test the visual apperance

--- lesson.js
+++ lesson.js
@@ -1,4 +1,4 @@
-let lesson = 
+let lesson = [
     {
          id: 101,
                     subject: 'Math',
@@ -7,4 +7,87 @@ let lesson =
                     image: "download.jpg",
                     spaces: 10,
                     rating: 3
-    }
+},
+{
+         id: 102,
+                    subject: 'English',
+                    location: 'London',
+                    price: 30,
+                    image: "download.jpg",
+                    spaces: 8,
+                    rating: 4
+},
+{
+         id: 103,
+                    subject: 'Science',
+                    location: 'London',
+                    price: 50,
+                    image: "download.jpg",
+                    spaces: 5,
+                    rating: 4
+},
+{
+         id: 104,
+                    subject: 'IT',
+                    location: 'Bath',
+                    price: 45,
+                    image: "download.jpg",
+                    spaces: 12,
+                    rating: 2
+},
+{
+         id: 105,
+                    subject: 'Chemistry',
+                    location: 'London',
+                    price: 42,
+                    image: "download.jpg",
+                    spaces: 10,
+                    rating: 3
+},
+{
+         id: 106,
+                    subject: 'Math',
+                    location: 'Bath',
+                    price: 40,
+                    image: "download.jpg",
+                    spaces: 15,
+                    rating: 3
+},
+{
+         id: 107,
+                    subject: 'Math',
+                    location: 'Liverpool',
+                    price: 60,
+                    image: "download.jpg",
+                    spaces: 10,
+                    rating: 1
+},
+{
+         id: 108,
+                    subject: 'Biology',
+                    location: 'Liverpool',
+                    price: 45,
+                    image: "download.jpg",
+                    spaces: 10,
+                    rating: 5
+},
+{
+         id: 109,
+                    subject: 'Chemistry',
+                    location: 'Liverpool',
+                    price: 40,
+                    image: "download.jpg",
+                    spaces: 10,
+                    rating: 1
+},
+{
+         id: 110,
+                    subject: 'Lab',
+                    location: 'London',
+                    price: 10,
+                    image: "download.jpg",
+                    spaces: 20,
+                    rating: 4
+}
+]
+    
