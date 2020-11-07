b5a2bfeb0da58edac73aad1ffccc00c7f103b3ff
laveshbhatia <lavesh2001@gmail.com>
2020-11-07 06:44:58 +0400

    Added Vue to the project and added a new file with one product for testing

---
+++ lesson.js
@@ -0,0 +1,10 @@
+let lesson = 
+    {
+         id: 101,
+                    subject: 'Math',
+                    location: 'London',
+                    price: 40,
+                    image: "download.jpg",
+                    spaces: 10,
+                    rating: 3
+    }
